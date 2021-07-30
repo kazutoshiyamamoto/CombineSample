e1e7d5dbfe9db0e1027267899ac2550a4cfa44c8
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-30 22:18:50 +0900

    今後使用する予定の実装をコメントアウト

--- CombineSample/CombineSample/ContentViewModel.swift
+++ CombineSample/CombineSample/ContentViewModel.swift
@@ -14,14 +14,35 @@ final class ContentViewModel: ObservableObject {
     
     private var cancellable: AnyCancellable?
     
+    //    private var cancellables: Set<AnyCancellable> = []
     
     init() {
         cancellable = $text
             .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
+            //            .filter( { $0.isAlphanumeric() } )
             .filter( { $0.hasPrefix("1") } ) // 実際に動かした時に挙動がわかりやすいため追加
             .debounce(for: .milliseconds(500), scheduler: RunLoop.main) // ユーザーの入力が停止するのを待つ
             .receive(on: RunLoop.main)
             .assign(to: \.filteredText, on: self)
         
+        //        $text
+        //            .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
+        //            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+        //            .receive(on: RunLoop.main)
+        //            .assign(to: \.filteredText, on: self)
+        //            .store(in: &cancellables)
+        
+        //        // Justの挙動チェック
+        //        cancellable = Just("test")
+        //            .sink { message in
+        //                print("message: \(message)")
+        //            }
     }
 }
+
+//extension String {
+//    // 半角数字の判定
+//    func isAlphanumeric() -> Bool {
+//        return self.range(of: "[^0-9]+", options: .regularExpression) == nil && self != ""
+//    }
+//}
