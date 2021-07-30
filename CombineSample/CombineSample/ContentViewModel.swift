e7365453a2cd40bcc5a2db0104b83ac23f462471
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-30 22:18:00 +0900

    CombineでTextFieldに入力された値の通知を処理する

--- CombineSample/CombineSample/ContentViewModel.swift
+++ CombineSample/CombineSample/ContentViewModel.swift
@@ -11,4 +11,17 @@ import Combine
 final class ContentViewModel: ObservableObject {
     @Published var text = ""
     @Published var filteredText = ""
+    
+    private var cancellable: AnyCancellable?
+    
+    
+    init() {
+        cancellable = $text
+            .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
+            .filter( { $0.hasPrefix("1") } ) // 実際に動かした時に挙動がわかりやすいため追加
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main) // ユーザーの入力が停止するのを待つ
+            .receive(on: RunLoop.main)
+            .assign(to: \.filteredText, on: self)
+        
+    }
 }
