8e5034aba29ca161b3c447758a8afd2a4b57ebed
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-08-03 22:44:32 +0900

    Setを使ってAnyCancellableを管理するサンプルを追加

--- CombineSample/CombineSample/ContentViewModel.swift
+++ CombineSample/CombineSample/ContentViewModel.swift
@@ -12,18 +12,53 @@ final class ContentViewModel: ObservableObject {
     @Published var text = ""
     @Published var filteredText = ""
     
-    private var cancellable: AnyCancellable?
+    @Published var textA = ""
     
-    //    private var cancellables: Set<AnyCancellable> = []
+    @Published var textB = ""
+    
+    @Published var textC = ""
+    
+    private var cancellables: Set<AnyCancellable> = []
     
     init() {
-        cancellable = $text
-            .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
+        $textA
+            //            .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
             //            .filter( { $0.isAlphanumeric() } )
-//            .filter( { $0.hasPrefix("1") } ) // 実際に動かした時に挙動がわかりやすいため追加
-            .debounce(for: .milliseconds(500), scheduler: RunLoop.main) // ユーザーの入力が停止するのを待つ
-            .receive(on: RunLoop.main)
-            .assign(to: \.filteredText, on: self)
+            //            .filter( { $0.hasPrefix("1") } ) // 実際に動かした時に挙動がわかりやすいため追加
+            //            .debounce(for: .milliseconds(500), scheduler: RunLoop.main) // ユーザーの入力が停止するのを待つ
+            //            .receive(on: RunLoop.main)
+            //            .assign(to: \.filteredText, on: self)
+            .sink(receiveValue: {_ in
+                // do something
+            })
+            .store(in: &cancellables)
+        
+        $textB
+            .sink(receiveValue: {_ in
+                // do something
+            })
+            .store(in: &cancellables)
+        
+        
+        //            .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
+        //            //            .filter( { $0.isAlphanumeric() } )
+        //            //            .filter( { $0.hasPrefix("1") } ) // 実際に動かした時に挙動がわかりやすいため追加
+        //            .debounce(for: .milliseconds(500), scheduler: RunLoop.main) // ユーザーの入力が停止するのを待つ
+        //            .receive(on: RunLoop.main)
+        //            .assign(to: \.filteredText, on: self)
+        
+        $textC
+            .sink(receiveValue: {_ in
+                // do something
+            })
+            .store(in: &cancellables)
+        
+        //            .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
+        //            //            .filter( { $0.isAlphanumeric() } )
+        //            //            .filter( { $0.hasPrefix("1") } ) // 実際に動かした時に挙動がわかりやすいため追加
+        //            .debounce(for: .milliseconds(500), scheduler: RunLoop.main) // ユーザーの入力が停止するのを待つ
+        //            .receive(on: RunLoop.main)
+        //            .assign(to: \.filteredText, on: self)
         
         //        $text
         //            .filter( { $0.unicodeScalars.allSatisfy( { CharacterSet.alphanumerics.contains($0) } ) } )
