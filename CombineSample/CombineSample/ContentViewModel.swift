b86b7f801060c55a051d9edfa03f5072936be730
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-08-22 16:09:18 +0900

    Timerの処理をCombineを使って置き換える

--- CombineSample/CombineSample/ContentViewModel.swift
+++ CombineSample/CombineSample/ContentViewModel.swift
@@ -12,7 +12,6 @@ final class ContentViewModel: ObservableObject {
     @Published var count = 0
     let end: Int = 4000000
     
-    private var cancellable: AnyCancellable?
     @Published var isTimerRunning = false
     
     init() {
@@ -52,17 +51,20 @@ final class ContentViewModel: ObservableObject {
                     self.cancellable?.cancel()
                 }
             }
+    private var cancellable: AnyCancellable?
     
     func startCounting() {
         isTimerRunning = true
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-            self.count += 1
-        }
+        cancellable = Timer.publish(every: 1.0, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                self.count += 1
+            }
     }
     
     func stopCounting() {
         isTimerRunning = false
-        timer?.invalidate()
+        cancellable?.cancel()
     }
     
     func resetCount() {
