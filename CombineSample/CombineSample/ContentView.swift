016a764ff350535c00fcd19fe8dcc1e078414a3e
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-09-08 21:13:55 +0900

    コメントアウトを解除

--- CombineSample/CombineSample/ContentView.swift
+++ CombineSample/CombineSample/ContentView.swift
@@ -29,8 +29,8 @@ struct ContentView: View {
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView(viewModel: ContentViewModel())
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(viewModel: ContentViewModel(searchUserModel: SearchUserModel()))
+    }
+}
