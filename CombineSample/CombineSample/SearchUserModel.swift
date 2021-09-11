21b57eca7e1b4383d29ce16f678560855c6275c9
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-09-11 21:10:27 +0900

    不要な実装を削除

--- CombineSample/CombineSample/SearchUserModel.swift
+++ CombineSample/CombineSample/SearchUserModel.swift
@@ -36,29 +36,3 @@ final class SearchUserModel: SearchUserModelProtocol {
         }
     }
 }
-
-//protocol SearchUserModelProtocol {
-//    func fetchUser(query: String) -> Observable<[User]>
-//}
-//
-//final class SearchUserModel: SearchUserModelProtocol {
-//    let session = Session()
-//
-//    func fetchUser(query: String) -> Observable<[User]> {
-//        return Observable.create { [weak self] observer in
-//            let request = SearchUsersRequest(query: query, sort: nil, order: nil, page: nil, perPage: nil)
-//            let task = self?.session.send(request) { result in
-//                switch result {
-//                case .success(let response):
-//                    observer.onNext(response.0.items)
-//                    observer.onCompleted()
-//                case .failure(let error):
-//                    observer.onError(error)
-//                }
-//            }
-//            return Disposables.create {
-//                task?.cancel()
-//            }
-//        }
-//    }
-//}
